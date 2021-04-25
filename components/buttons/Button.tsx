00623378ddf903a55a8f6dfcb824b9043495d73e
Victor Timsit <victortimsit.contact@gmail.com>
2021-04-25 20:32:57 +0200

    Added controls on FloatFace

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -5,14 +5,24 @@ export default function Button({
   onClick,
   children,
   icon,
+  className,
+  size = "medium",
 }: {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   children?: string;
   icon?: ReactElement;
+  className?: string;
+  size?: "medium" | "small";
 }) {
   return (
     <button
-      className="px-3 py-1 bg-primary-500 flex align-center items-center rounded-lg h-12 focus:outline-none active:bg-primary-900 hover:bg-primary-400"
+      className={`${
+        children && "px-3"
+      } bg-primary-500 flex justify-center items-center rounded-lg ${
+        size == "medium" ? "h-12" : "h-8"
+      } 
+      ${children ? "w-auto" : size == "medium" ? "w-12" : "w-8"}
+      focus:outline-none active:bg-primary-900 hover:bg-primary-400 ${className}`}
       onClick={onClick}
     >
       {icon && cloneElement(icon, { className: "h-5 w-5 text-neutral-50" })}
--- components/feedbacks/FloatFace.tsx
+++ components/feedbacks/FloatFace.tsx
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+} from "@heroicons/react/solid";
+import React, { useRef, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 import Webcam from "react-webcam";
+import Button from "../buttons/Button";
 
 const Position = {
   x: {
@@ -14,36 +21,76 @@ const Position = {
 };
 
 export default function Floatface(props) {
+  const [controls, setControls] = useState(true);
   const [position, setPosition] = useState({
     x: Position.x.left,
     y: Position.y.bottom,
   });
-  let oldPos = { x: null, y: null };
+  let oldPos = useRef({ x: Position.x.left, y: Position.y.bottom });
 
-  useHotkeys("cmd+left", (e) => {
+  const handlePosition = (e: any, position: { x?: string; y?: string }) => {
     e.preventDefault();
-    setPosition({ x: Position.x.left, y: oldPos.y });
-    oldPos.x = Position.x.left;
-  });
-  useHotkeys("cmd+right", (e) => {
-    e.preventDefault();
-    setPosition({ x: Position.x.right, y: oldPos.y });
-    oldPos.x = Position.x.right;
-  });
-  useHotkeys("cmd+up", (e) => {
-    e.preventDefault();
-    setPosition({ x: oldPos.x, y: Position.y.top });
-    oldPos.y = Position.y.top;
-  });
-  useHotkeys("cmd+down", (e) => {
-    e.preventDefault();
-    setPosition({ x: oldPos.x, y: Position.y.bottom });
-    oldPos.y = Position.y.bottom;
-  });
+
+    setPosition({
+      x: position.x ? position.x : oldPos.current.x,
+      y: position.y ? position.y : oldPos.current.y,
+    });
+    if (position.x) oldPos.current.x = position.x;
+    if (position.y) oldPos.current.y = position.y;
+  };
+
+  useHotkeys("cmd+left", (e) => handlePosition(e, { x: Position.x.left }));
+  useHotkeys("cmd+right", (e) => handlePosition(e, { x: Position.x.right }));
+  useHotkeys("cmd+up", (e) => handlePosition(e, { y: Position.y.top }));
+  useHotkeys("cmd+down", (e) => handlePosition(e, { y: Position.y.bottom }));
   return (
-    <Webcam
-      className={`${position.x} ${position.y} rounded-full shadow-2xl fixed transition-all`}
-      videoConstraints={{ width: 200, height: 200 }}
-    />
+    <div
+      className={`${position.x} ${position.y} fixed`}
+      onMouseEnter={() => setControls(true)}
+      onMouseLeave={() => setControls(false)}
+    >
+      <Webcam
+        className="rounded-full shadow-2xl m-2"
+        videoConstraints={{ width: 200, height: 200 }}
+      />
+      <div
+        className={`flex items-center justify-center transition-opacity ${
+          controls ? "opacity-1" : "opacity-0"
+        } `}
+      >
+        <Button
+          size="small"
+          className={`
+        ${position.y == Position.y.top ? "opacity-50" : "opacity-1"} 
+        transition-opacity absolute -top-8`}
+          icon={<ChevronUpIcon />}
+          onClick={(e) => handlePosition(e, { y: Position.y.top })}
+        />
+        <Button
+          size="small"
+          className={`
+          ${position.x == Position.x.left ? "opacity-50" : "opacity-1"}
+          transition-opacity absolute -left-8 top-1/2 transform -translate-y-1/2`}
+          icon={<ChevronLeftIcon />}
+          onClick={(e) => handlePosition(e, { x: Position.x.left })}
+        />
+        <Button
+          size="small"
+          className={`
+            ${position.x == Position.x.right ? "opacity-50" : "opacity-1"}
+            transition-opacity absolute top-1/2 -right-8 transform -translate-y-1/2`}
+          icon={<ChevronRightIcon />}
+          onClick={(e) => handlePosition(e, { x: Position.x.right })}
+        />
+        <Button
+          size="small"
+          className={`
+          ${position.y == Position.y.bottom ? "opacity-50" : "opacity-1"}
+          transition-opacity absolute -bottom-8`}
+          icon={<ChevronDownIcon />}
+          onClick={(e) => handlePosition(e, { y: Position.y.bottom })}
+        />
+      </div>
+    </div>
   );
 }
