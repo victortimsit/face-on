f83d023aa07b9e567d6f979550afa7f013979dfb
Victor Timsit <victortimsit.contact@gmail.com>
2021-04-27 16:49:36 +0200

    :bug: export of getHeaders

--- pages/api/getHeaders.js
+++ pages/api/getHeaders.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async (req, res) => {
   try {
     const _res = await fetch(req.body.url, { method:"HEAD" });
     res.json({headers: _res.headers})
