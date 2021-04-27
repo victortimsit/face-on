c9ca133bdb2466f10318952169e733a915596f56
Victor Timsit <victortimsit.contact@gmail.com>
2021-04-27 16:22:00 +0200

    :alembic: API getHeaders func

--- pages/api/xFramePermission.ts
+++
@@ -1,9 +0,0 @@
-const xFramePermission = async (url: string) => {
-  try {
-    const res = await fetch(url, {method: "HEAD"});
-    return res;
-  } catch (error) {
-    console.log(error);
-  } 
-}
-export default xFramePermission;
