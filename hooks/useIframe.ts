6843f22dba6f79e7b0afa896d31b4b596f3cba43
victortimsit <victortimsit.contact@gmail.com>
2021-09-26 14:39:09 +0200

    :sparkles: Added youtube iframe support

--- hooks/useIframe.ts
+++ hooks/useIframe.ts
@@ -4,11 +4,13 @@ import getXFrameOptions from "../services/getXFrameOptions";
 enum ServiceURL {
   GOOGLE_SLIDE = "docs.google.com",
   FIGMA = "www.figma.com",
+  YOUTUBE = "www.youtube.com",
 }
 
 enum ServiceEmbedPath {
   GOOGLE_SLIDE = "/embed?start=false",
   FIGMA = "/embed?embed_host=share&url=",
+  YOUTUBE = "/embed/",
 }
 
 enum ServiceUnwantedPath {
@@ -35,16 +37,26 @@ const getFigmaIframe = (url: string) => {
   );
 };
 
+const getYoutubeIframe = (url: string) => {
+  const urlParams = new URLSearchParams(new URL(url).search);
+  const vParam = urlParams.get("v");
+
+  return "https://" + ServiceURL.YOUTUBE + ServiceEmbedPath.YOUTUBE + vParam;
+};
+
 const useIframe = () => {
   const [loadedIframe, setLoadedIframe] = useState(null);
   const loadIframe = async (url: string) => {
     if (url) {
       // setLoadedIframe(null);
       // if(!url.includes("http")) return false
+
       const iframeUrl: string = url.includes(ServiceURL.GOOGLE_SLIDE)
         ? getGoogleIframe(url)
         : url.includes(ServiceURL.FIGMA)
         ? getFigmaIframe(url)
+        : url.includes(ServiceURL.YOUTUBE)
+        ? getYoutubeIframe(url)
         : url;
 
       try {
